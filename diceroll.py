8edb887c2a02c604adcdad33f67083b28537c983
David Tran <davidtran@Davids-MacBook-Air.local>
2026-07-20 12:53:04 -0700

    Add diceroll.py

--- diceroll.py
+++ diceroll.py
@@ -1,6 +1,10 @@
 #calculates the number of times the sum of the randomly rolled dice equals each possible value from 2 to 12.
-#repeatedly asks the user for the number of times to roll the dice, quitting only when the user-entered number is less than 1. Hint: Use a while loop that will execute as long as num_rolls is greater than or equal to 1.
-#prints a histogram in which the total number of times the dice rolls equals each possible value and is displayed by printing a character, such as *, that number of times. Ex:
+#repeatedly asks the user for the number of times to roll the dice,
+# quitting only when the user-entered number is less than 1.
+# Hint: Use a while loop that will execute as long as num_rolls is greater than or equal to 1.
+#prints a histogram in which the total number of times the dice rolls equals each possible value
+# and is displayed by printing a character, such as *,
+# that number of times. Ex:
 #Dice roll histogram:
 # 2s:  **
 # 3s:  ****
